868819ee176a0252d0368ebc21fbdf5da050ca79
Kevin Liddle <kevin@8thlight.com>
2015-11-11 21:34:29 -0600

    fix missing question mark. thanks rob. jerk.

--- building-personal-property.js
+++ building-personal-property.js
@@ -1,5 +1,5 @@
 document.onkeyup = function(e) {
   if (e.target.value && e.target.value.match(/building personal property/i)) {
-    alert("You typed \"Building Personal Property\". Did you mean \"Business Personal Property\"");
+    alert("You typed \"Building Personal Property\". Did you mean \"Business Personal Property\"?");
   }
 };
